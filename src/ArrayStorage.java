1f12a3d8d4b4e4aa8de932793dc02af509b92e04
alxndrfmn <alxndfmn@gmail.com>
2020-04-05 19:26:46 +0400

    Реализация класса ArrayStorage  в сооответствии с заданием. Метод getDummyResume... - для формирования осмысленного сообщения при попытке выборки несуществующего резюме (...использовать в delete(), save()). Mетод save() учитывает возможность переполнения массива Resume[] storage без выброса ArrayBoundsException.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,64 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    static int size = 0;
+    static int capacity = 4;
+    Resume[] storage = new Resume[capacity];
 
     void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
     void save(Resume r) {
+        if (size < capacity) {
+            storage[size++] = r;
+        } else {
+            System.out.println("ArrayStorage: save attempt -" + r + "- out of storage memory limit!");
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        for (Resume resume : getAll()) {
+            if (resume.uuid == uuid) {
+                return resume;
+            }
+        }
+        return getDummyResume(uuid);
+    }
+
+    Resume getDummyResume(String uuid) {
+        Resume dummyResume = new Resume();
+        dummyResume.uuid = uuid + " - doesn't exist!";
+        return dummyResume;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                for (int j = i; j < size - 1; j ++) {
+                    storage[j] = storage[j + 1];
+                }
+                storage[size - 1] = null;
+                size--;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
