526fd498b2c57787ef8fa96566cd16ffd932e70b
Alexander Fomin <afomin@mail.ru>
2020-06-03 16:08:52 +0400

    Lesson #7:

--- src/com/urise/webapp/model/OrganizationSection.java
+++ src/com/urise/webapp/model/OrganizationSection.java
@@ -1,5 +1,6 @@
 package com.urise.webapp.model;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class OrganizationSection extends Section{
@@ -11,16 +12,18 @@ public class OrganizationSection extends Section{
 
     @Override
     public void printSection(SectionType sectionType, Section section) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/YYYY");
         System.out.println(sectionType.getTitle());
         System.out.println();
         for (Organization organization : organizations) {
             System.out.println(organization.getName());
             for (Organization.Job job : organization.getJobs()) {
-                System.out.println
-                        (job.getStartDate().getMonthValue() + "/" + job.getStartDate().getYear() + " - " +
-                        (job.getFinishDate().toEpochDay() == 0 ? "Сейчас" : job.getFinishDate().getMonthValue()+ "/" + job.getFinishDate().getYear()) + "    " +
+                System.out.println(
+                        job.getStartDate().format(dtf) + " - " +
+                        (job.getFinishDate().toEpochDay() == 0 ? "Сeйчас" : job.getFinishDate().format(dtf)) + "   " +
                         job.getJobName() +
-                        (job.getJobDescription().equals("") ? "\r" :  "\n" + job.getJobDescription()));
+                        (job.getJobDescription().equals("") ? "\r" :  "\n" + job.getJobDescription())
+                );
             }
         }
         printSectionDelimeter();
